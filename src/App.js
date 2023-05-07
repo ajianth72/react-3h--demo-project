741c8bf5a2f278ae8a3d258d108b5ed195573e9b
ajinath72 <ajinathpawar72@gmail.com>
2023-05-07 15:13:57 +0530

    complete-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form/Form";
+import { useState } from "react";
+import ProductItem from "./components/Products/ProductItem";
+import CartContext from "./components/Contexts/Cartcontext";
+import CartModal from "./components/Modal/CartModal";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const[productList , setProductList] = useState([]);
+  const [orderList , setOrderList] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const ctxObj = {
+    orderList : orderList ,
+    setOrderList: setOrderList ,
+    productList : productList ,
+    cartVisibility: isModalOpen,
+    setCartVisibility: setIsModalOpen,
+    setProductList : setProductList
+  }
+  const formSubmitHandler = (value) => {
+    setProductList((prevList)=>{
+      return [value , ...prevList]
+    })
+  } 
+  
+  const ShowProducts = productList.map((product)=>{
+   return  <ProductItem key = {product.id} item ={product} ></ProductItem>
+  })
+  let cartItemCount = 0 ;
+  orderList.forEach((item)=>{
+    cartItemCount += (item.quantityL + item.quantityM + item.quantityS) 
+  })
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <CartContext.Provider value = {ctxObj}>
+      {isModalOpen && <CartModal>
+      <Cart></Cart>
+      </CartModal> } 
+      <button style={{float:'right'}} onClick={()=>setIsModalOpen(true)}>{`My Cart ${cartItemCount}`} </button>
+      <Form onSubmit = {formSubmitHandler}></Form>
+      {ShowProducts}
+      </CartContext.Provider>
+      </>
+       
   );
 }
 
